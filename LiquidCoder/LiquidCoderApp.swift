b6b77032df4ad054657613ee299f87e5274716ac
Reom <reomnagasaka@gmail.com>
2026-04-30 09:10:05 +0900

    Initial Commit

---
+++ LiquidCoder/LiquidCoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidCoderApp.swift
+//  LiquidCoder
+//
+//  Created by Reom Nagasaka on 2026/04/30.
+//
+
+import SwiftUI
+
+@main
+struct LiquidCoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
